8945fbfbd1fee69bf04579b8bdc3a8cc7fa700fe
Vladimir Mironjuk <vladimironiuk@gmail.com>
2021-12-25 20:21:30 +0200

    Prove that 'AddMember' method stores two members on adding two members in the same order

--- server/member_club_store_test.go
+++ server/member_club_store_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
@@ -17,5 +18,28 @@ func TestAddMemberStoresNewMember(t *testing.T) {
 
 	if mcs.members[0] != expectedMember {
 		t.Errorf("Expected %v, got %v instead\n", expectedMember, mcs.members[0])
+	}
+}
+
+func TestAddMemberStoresTwoMembersOnAddingTwoMembers(t *testing.T) {
+	mcs := MemberClubStore{}
+	expectedMembers := []Member{
+		Member{
+			name:      "member1",
+			email:     "member1@example.com",
+			dateAdded: time.Now(),
+		},
+		Member{
+			name:      "member2",
+			email:     "member2@example.com",
+			dateAdded: time.Now(),
+		},
+	}
+
+	mcs.AddMember(expectedMembers[0])
+	mcs.AddMember(expectedMembers[1])
+
+	if !reflect.DeepEqual(mcs.members, expectedMembers) {
+		t.Errorf("Expected %v, got %v instead\n", expectedMembers, mcs.members)
 	}
 }
