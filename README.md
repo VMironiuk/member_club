93f4c8f68b477cfd40171faf174e024afc2086ab
Vladimir Mironjuk <vladimironiuk@gmail.com>
2021-12-26 14:45:34 +0200

    Add Gin web framework

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 [![Go](https://github.com/VMironiuk/member_club/actions/workflows/CI.yml/badge.svg)](https://github.com/VMironiuk/member_club/actions/workflows/CI.yml)
+
+- Install Gin package:
+```
+$ go get -u github.com/gin-gonic/gin
+```
--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", rootRequest)
+	r.Run()
+}
+
+func rootRequest(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
