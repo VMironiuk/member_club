c3de1ddee5a31003c4d9a3196bf73ecbac9b4e78
Vladimir Mironjuk <vladimironiuk@gmail.com>
2021-12-26 15:25:33 +0200

    Send list of test members

--- main.go
+++ main.go
@@ -10,21 +10,38 @@ import (
 var inMemoryStore = store.InMemoryMemberClubStore{}
 
 func main() {
+	fillTestMembers()
+
 	r := gin.Default()
-	inMemoryStore.AddMember(store.Member{
-		Name:      "John Smith",
-		Email:     "john.smith@gmail.com",
-		DateAdded: time.Now(),
-	})
 	r.GET("/", rootRequest)
 	r.Run()
 }
 
 func rootRequest(c *gin.Context) {
-	member := inMemoryStore.GetMembers()[0]
 	c.JSON(200, gin.H{
-		"name":      member.Name,
-		"email":     member.Email,
-		"dateAdded": member.DateAdded,
+		"members": inMemoryStore.GetMembers(),
+	})
+}
+
+func fillTestMembers() {
+	inMemoryStore.AddMember(store.Member{
+		Name:      "John Show",
+		Email:     "j.show@company.com",
+		DateAdded: time.Now(),
+	})
+	inMemoryStore.AddMember(store.Member{
+		Name:      "Dana A. Watkins",
+		Email:     "Dana2000@mail.com",
+		DateAdded: time.Now(),
+	})
+	inMemoryStore.AddMember(store.Member{
+		Name:      "Henri Rousseau",
+		Email:     "rousseau@impressionis.fr",
+		DateAdded: time.Now(),
+	})
+	inMemoryStore.AddMember(store.Member{
+		Name:      "Mr. Brown",
+		Email:     "brown@gmail.com",
+		DateAdded: time.Now(),
 	})
 }
