e4e7126d4d51e413ec76ac0d6e58e28a3286fde8
Vladimir Mironjuk <vladimironiuk@gmail.com>
2021-12-26 12:50:18 +0200

    Move 'InMemoryMemberClubStore' to its own file

--- member_club_store.go
+++ member_club_store.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/mail"
-	"regexp"
 	"time"
 )
 
@@ -15,47 +13,4 @@ type Member struct {
 type MemberClubStore interface {
 	AddMember(mmember Member)
 	GetMembers() []Member
-}
-
-type InMemoryMemberClubStore struct {
-	members []Member
-}
-
-func (store *InMemoryMemberClubStore) AddMember(member Member) {
-	if !isNameValid(member.name) {
-		return
-	}
-
-	if !isValidEmail(member.email) {
-		return
-	}
-
-	if store.containsEmail(member.email) {
-		return
-	}
-
-	store.members = append(store.members, member)
-}
-
-func (store *InMemoryMemberClubStore) GetMembers() []Member {
-	return store.members
-}
-
-func (store *InMemoryMemberClubStore) containsEmail(email string) bool {
-	for _, m := range store.members {
-		if m.email == email {
-			return true
-		}
-	}
-	return false
-}
-
-func isValidEmail(email string) bool {
-	_, err := mail.ParseAddress(email)
-	return err == nil
-}
-
-func isNameValid(name string) bool {
-	r := regexp.MustCompile(`^[a-zA-Z \.]+$`)
-	return r.MatchString(name)
 }
